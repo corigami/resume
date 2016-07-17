f572de50699bc49193c448eff921678389fc3f40
Corey Willinger <corey.willinger@gmail.com>
2016-07-16 21:48:12 -0400

    added initial files

---
+++ js/helper.js
@@ -0,0 +1,182 @@
+/*global helperVar, controller, google*/
+//helper variables for BIO block of resumeBuilder.js
+var helperVar = {
+    HTMLheaderName: '<h1 id="name">%data%</h1>',
+    HTMLheaderRole: '<span>%data%</span><hr/>',
+    HTMLcontactGeneric: '<li class="flex-item"><span class="lt-green-text">%contact%</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLmobile: '<li class="flex-item"><span class="lt-green-text">mobile</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLemail: '<li class="flex-item"><span class="lt-green-text">email</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLtwitter: '<li class="flex-item"><span class="lt-green-text">twitter</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLgithub: '<li class="flex-item"><span class="lt-green-text">github</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLblog: '<li class="flex-item"><span class="lt-green-text">blog</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLlocation: '<li class="flex-item"><span class="lt-green-text">location</span><span class="lt-blue-text">%data%</span></li>',
+    HTMLbioPic: '<img src="%data%" class="biopic">',
+    HTMLwelcomeMsg: '<span class="welcome-message">%data%</span>',
+    HTMLskillsStart: '<h3 id="skills-h3" class="skills-start">Skills at a Glance:</h3><ul id="skills" class="flex-box"></ul>',
+    HTMLskills: '<li class="flex-item"><span class="lt-blue-text">%data%</span></li>',
+
+    //helper variables for WORK block of resumeBuilder.js
+    HTMLworkStart: '<div class="work-entry"></div>',
+    HTMLworkEmployer: '<a href="#">%data%',
+    HTMLworkTitle: ' - %data%</a>',
+    HTMLworkDates: '<div class="date-text">%data%</div>',
+    HTMLworkLocation: '<div class="location-text">%data%</div>',
+    HTMLworkDescStart: '<div class="workDesc-entry"><br><UL></div>',
+    HTMLworkDescription: '<LI><p>%data%</p>',
+    HTMLworkDescEnd: '</UL>',
+
+    //helper variables for PROJECT block of resumeBuilder.js
+    HTMLprojectStart: '<div class="project-entry"></div>',
+    HTMLprojectTitle: '<a href="%url%">%data%</a>',
+    HTMLprojectDates: '<div class="date-text">%data%</div>',
+    HTMLprojectDescription: '<p><br>%data%</p>',
+    HTMLprojectImage: '<img src="%data%">',
+    HTMLprojectGiturl: '<a href="#2">%data%</a><br>',
+    HTMLprojectEmbed: '<div class="embed-container">%data</div>',
+
+    //helper variables for EDUCATION block of resumeBuilder.js
+    HTMLschoolFormalStart: '<h3 class="education-entry">Formal Education<br></h3>',
+    HTMLschoolStart: '<div class="education-entry"></div>',
+    HTMLschoolName: '<a href="%url%">%data%',
+    HTMLschoolDegree: ' -- %data%</a>',
+    HTMLschoolDates: '<div class="date-text">%data%</div>',
+    HTMLschoolLocation: '<div class="location-text">%data%</div>',
+    HTMLschoolMajor: '<em><br>Major: %data%</em>',
+    HTMLonlineClasses: '<div class="education-entry"><br><strong>Certificates and Other Relevent Courses<strong><br></div>',
+    HTMLonlineSchool: '<a href="%url%">%data%</a>',
+    HTMLonlineTitle: '<div><p>%data%</p></div>',
+    HTMLonlineDates: '<div class="date-text">%data%</div><br><br>',
+
+
+    //variables used for map generation
+    googleMap: '<div id="map" class="map"></div>',
+    map: '' // declares a global map variable
+};
+
+function initializeMap() {
+    'use strict';
+    var locations, mapOptions;
+    mapOptions = {
+        disableDefaultUI: true
+    };
+    helperVar.map = new google.maps.Map(document.querySelector('#map'), mapOptions);
+    /*
+    locationFinder() returns an array of every location string from the JSONs
+    written for bio, education, and work.
+    */
+    function locationFinder() {
+        var locations = [],
+            BIO = controller.getBio(),
+            EDUCATION = controller.getEd(),
+            WORK = controller.getWork();
+        //adds the single location property from BIO to the locations array
+
+        locations.push(BIO.CONTACTS.LOCATION);
+
+        // iterates through school and work locations and appends each location to
+        // the locations array.  Since Google textSearch api's limits response to 10 values,
+        // logic has been added to not add duplicates
+        //  EDUCATION.SCHOOLS.forEach(function (school) {
+        //            //    if (locations.indexOf(school.LOCATION) == -1) {
+        //  locations.push(school.LOCATION);
+        //                 }
+        //   });
+        WORK.JOBS.forEach(function (job) {
+            //        if (locations.indexOf(job.LOCATION) == -1) {
+            locations.push(job.LOCATION);
+            //       }
+        });
+        return locations;
+    }
+
+    /*
+    createMapMarker(placeData) reads Google Places search results to create map pins.
+    placeData is the object returned from search results containing information
+    about a single location.
+    */
+    function createMapMarker(placeData) {
+        // The next lines save location data from the search result object to local variables
+        var lat = placeData.geometry.location.lat(), // latitude from the place service
+            lon = placeData.geometry.location.lng(), // longitude from the place service
+            name = placeData.formatted_address, // name of the place from the place service
+            bounds = window.mapBounds, // current boundaries of the map window
+            // marker is an object with additional data about the pin for a single location
+            marker = new google.maps.Marker({
+                map: helperVar.map,
+                position: placeData.geometry.location,
+                title: name
+            }),
+            // infoWindows are the little helper windows that open when you click
+            // or hover over a pin on a map. They usually contain more information
+            // about a location.
+            infoWindow = new google.maps.InfoWindow({
+                content: '<div id="markerName">' +
+                    '<b>' + name + '</b>' +
+                    '</div>'
+            });
+
+        google.maps.event.addListener(marker, 'click', function () {
+            infoWindow.open(helperVar.map, marker);
+        });
+
+        // this is where the pin actually gets added to the map.
+        // bounds.extend() takes in a map location object
+        bounds.extend(new google.maps.LatLng(lat, lon));
+        helperVar.map.fitBounds(bounds); // fit the map to the new marker
+        helperVar.map.setCenter(bounds.getCenter()); // center the map
+    }
+
+    /*
+    callback(results, status) makes sure the search returned results for a location.
+    If so, it creates a new map marker for that location.
+    */
+    function callback(results, status) {
+        if (status == google.maps.places.PlacesServiceStatus.OK) {
+            createMapMarker(results[0]);
+        }
+    }
+
+    /*
+    pinPoster(locations) takes in the array of locations created by locationFinder()
+    and fires off Google place searches for each location
+    */
+    function pinPoster(locations) {
+
+        // creates a Google place search service object. PlacesService does the work of
+        // actually searching for location data.
+        var service = new google.maps.places.PlacesService(helperVar.map);
+
+        // Iterates through the array of locations, creates a search object for each location
+        locations.forEach(function (place) {
+
+            // the search request object
+            var request = {
+                query: place
+            };
+
+            // Actually searches the Google Maps API for location data and runs the callback
+            // function with the search results after each search.
+            service.textSearch(request, callback);
+        });
+    }
+
+    // Sets the boundaries of the map based on pin locations
+    window.mapBounds = new google.maps.LatLngBounds();
+
+    // locations is an array of location strings returned from locationFinder()
+    locations = locationFinder();
+
+    // pinPoster(locations) creates pins on the map for each location in
+    // the locations array
+    pinPoster(locations);
+}
+
+// Calls the initializeMap() function when the page loads
+window.addEventListener('load', initializeMap);
+
+// Vanilla JS way to listen for resizing of the window and adjust map bounds
+window.addEventListener('resize', function (e) {
+    'use strict';
+    //  Make sure the map bounds get updated on page resize
+    helperVar.map.fitBounds(window.mapBounds);
+});
---
+++ js/model.js
@@ -0,0 +1,343 @@
+var model = {
+    BIO: {
+        NAME: 'Corey Willinger',
+        ROLE: 'Web Developer in training',
+        CONTACTS: {
+            EMAIL: 'corey.willinger@gmail.com',
+            MOBILE: '',
+            GITHUB: 'corigami',
+            LOCATION: 'Dayton, OH, USA'
+        },
+        PICTURE: 'images/me.jpg',
+        WELCOME_MSG: '',
+        SKILLS: [
+            'Software Engineering',
+            'Security+ Certified',
+            'DoD Certified Acquisition Professional (IT Level I)',
+            'Java',
+            'HTML5',
+            'Javascript',
+            'C++',
+            'C#',
+            'Distance Learning',
+            'Distributed Systems',
+            'Information Assurance',
+            'Computer Networking',
+            'SharePoint',
+            'MS Office',
+            'Military Veteran',
+            'Program/Project Management',
+            'Asset Management',
+            'Avionic Systems Repair',
+            'Electronic Priniciples'
+        ]
+    },
+
+    WORK: {
+        JOBS: [
+            {
+                TITLE: 'Course Director - Software Professional Development Program',
+                DESCRIPTION: [
+                    'Develops, directs & teaches instructor-led distance learning on software testing and software requirements',
+                    'Collaborates, develops and reviews course content for lessons covering all phases of software development',
+                    'Provides consulting to USAF and DoD software acquisition/sustainment programs & systems engineering functions',
+                    'Conducts applied research and communicates results in cooperation with software engineers and project managers',
+                    'Develops course content through Instructional Systems Design processes and creative delivery techniques'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2015-Present',
+                LOCATION: 'Wright-Patterson AFB, OH, USA'
+            }, {
+                TITLE: 'Branch Chief, Network Infrastructure / Plans & Programs Division Chief',
+                DESCRIPTION: [
+                    'Led 9 civ/mil personnel in Plans & Prog., budget, and Information Assurance',
+                    'Managed $1 Mil budget, established processes, reviewed IT requirements',
+                    'Directed and managed .edu network Certification and Accreditation',
+                    'Managed educational network infrastructure, including VoIP',
+                    'Installed/configured/managed CISCO TelePresence VTC system'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2011-2013',
+                LOCATION: 'Wright-Patterson AFB, OH, USA'
+            }, {
+                TITLE: 'Officer in Charge, Client Systems Branch / Project Manager',
+                DESCRIPTION: [
+                    'Led 90 mil/civilian personnel in Client Support, Land Mobile Radio, and Plans and Programs work centers',
+                    'Coordinated network maintenance actions, outages, and upgrades',
+                    'Managed Special Operations Forces Air Joint Operations Help Desk, VTC support of Iraq, Afghanistan Theater',
+                    'Managed $25 Million in Wing IT assets'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2007-2008',
+                LOCATION: 'Hurlburt Field, FL, USA'
+            }, {
+                TITLE: 'Wing Equipment Control Officer',
+                DESCRIPTION: [
+                    'Managed 105 IT Equipment Custodians, spanning 54 accounts',
+                    'Procured IT assets for the wing through purchasing channels and no-cost lateral resources',
+                    'Ensured compliance with federal and Air Force mandates for software managemen',
+                    'Coordinated telecommunication action requests with host base unit',
+                    'Managed $5 million in Wing IT assets'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2012-2013',
+                LOCATION: 'Hill AFB, UT, USA'
+            }, {
+                TITLE: 'Automated Test Station Production Supervisor',
+                DESCRIPTION: [
+                    'Supervised maintenance actions of 42 personnel on Improved Avionics Test Station and ' +
+                    'F-16 Line Replaceable Units for 66 assigned aircraft',
+                    'Provided extensive troubleshooting knowledge and training for complex malfunctions',
+                    'Established work methods, production controls, and performance standards',
+                    'Ensured section compliance with safety, environmental, and security programs',
+                    'Evaluated team leader and team member production skills, aptitude, and proficiency',
+                    'Managed squadron and section level Data Integrity Team program for maintenance documentation accuracy',
+                    'Managed $18 million of test and support equipment'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2005-2007',
+                LOCATION: 'Hill AFB, UT, USA'
+            }, {
+                TITLE: 'Avionics Intermediate Section Production Supervisor',
+                DESCRIPTION: [
+                    'Led maintenance actions of 11 personnel on Improved Avionics Test Station, A-10 and F-16 Line Replaceable units',
+                    'Established work methods, production controls and performance standards for assigned tasks',
+                    'Ensured compliance with safety, environmental, and security programs',
+                    'Managed $5.2 million of test and support equipment',
+                    'Led flight control center during exercise and contingency operations'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2004-2005',
+                LOCATION: 'Osan AB, South Korea'
+            }, {
+                TITLE: 'Avionics Intermediate Shop Team Leader',
+                DESCRIPTION: [
+                    'Ensured four repair team members were qualified to perform assigned tasks',
+                    'Managed and directed work effort of the repair team; was responsible for the quality of maintenance actions',
+                    'Ensured assigned equipment was properly maintained, repaired and calibrated',
+                    'Maintained, programmed and performed Time Compliant Technical Orders on avionic components ' +
+                    'specific to assigned test stations and support equipment'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '2001-2004',
+                LOCATION: 'Luke AFB, AZ, USA'
+            }, {
+                TITLE: 'Avionics Intermediate Shop Team Member',
+                DESCRIPTION: [
+                    'Operated, troubleshoot, repaired, inspected, and calibrated three Radar Electronic Warfare and ' +
+                    'two Digital, Analog and Video (DAV) test stations',
+                    'Performed maintenance, calibration, and operation checkout on Line Replaceable Units for 31 assigned B-1B aircraft',
+                    'Managed DAV section Electronic Static Discharge program',
+                    'Operated $8.1 million of test and support equipment'
+                ],
+                EMPLOYER: 'USAF',
+                DATES: '1998-2001',
+                LOCATION: 'Ellsworth AFB, SD, USA'
+            }
+        ]
+
+    },
+
+    EDUCATION: {
+        SCHOOLS: [
+            {
+                NAME: 'Air Force Institute of Technology',
+                DEGREE: 'Masters',
+                MAJOR: 'Computer Science (Software Engineering)',
+                DATES: '2013 - 2015',
+                LOCATION: 'Wright-Patterson AFB, OH, USA',
+                URL: 'http://www.afit.edu'
+            }, {
+                NAME: 'Park University',
+                DEGREE: 'Bachelors (Summa Cum Laude)',
+                MAJOR: 'Computer Science',
+                DATES: '2002 - 2007',
+                LOCATION: 'Hill AFB, UT',
+                URL: 'http://www.park.edu'
+            }
+        ],
+        ONLINE: [
+            {
+                SCHOOL: 'Udacity',
+                TITLE: 'Nano-degree: Android Development',
+                DATES: '2016-Present',
+                URL: 'http://www.udacity.com'
+            },
+            {
+                SCHOOL: 'Udacity',
+                TITLE: 'Nano-degree: Front-end Web Development',
+                DATES: 'June 2015- Jan 2016',
+                URL: 'http://www.udacity.com'
+            },
+            {
+                SCHOOL: 'Comptia',
+                TITLE: 'Security+ Certification',
+                DATES: 'Valid: Jul 2013 - Present',
+                URL: 'https://certification.comptia.org'
+            }, {
+                SCHOOL: 'Air Force Training Command',
+                TITLE: 'Cyberspace Officer Network Training (CCNA based) - (117 Hours)',
+                DATES: 'Apr 2012',
+                URL: '#'
+            },
+            {
+                SCHOOL: 'Air Force Training Command',
+                TITLE: 'Basic Communications Officer Training - (208 Hours)',
+                DATES: 'May 2009',
+                URL: '#'
+            },
+            {
+                SCHOOL: 'Aviation and Electronic Schools of America',
+                TITLE: 'FCC General Radiotelephone Operator\'s License \\w Radar Endorsement',
+                DATES: 'Nov 2002',
+                URL: '#'
+            },
+            {
+                SCHOOL: 'Air Force Training Command',
+                TITLE: 'Electronic Principles - (504 Hours)',
+                DATES: 'Apr 1998',
+                URL: '#'
+            }
+
+
+        ],
+        display: function () {
+            'use strict';
+            EDUCATION.SCHOOLS.forEach(function (school) {
+                var FORMATTED_NAME = helperVar.HTMLschoolName.replace('%data%', school.NAME),
+                    FORMATTED_DEGREE = helperVar.HTMLschoolDegree.replace('%data%', school.DEGREE),
+                    FORMATTED_MAJOR = helperVar.HTMLschoolMajor.replace('%data%', school.MAJOR),
+                    FORMATTED_DATES = helperVar.HTMLschoolDates.replace('%data%', school.DATES),
+                    FORMATTED_LOCATION = helperVar.HTMLschoolLocation.replace('%data%', school.LOCATION);
+                $('#education').append(helperVar.HTMLschoolStart);
+                $('.education-entry:last').append(FORMATTED_NAME);
+                $('.education-entry:last').append(FORMATTED_DATES);
+                $('.education-entry:last').append(FORMATTED_MAJOR);
+                $('.education-entry:last').append(FORMATTED_DEGREE);
+                $('.education-entry:last').append(FORMATTED_LOCATION);
+            });
+            EDUCATION.ONLINE.forEach(function (online) {
+                var FORMATTED_SCHOOL = helperVar.HTMLonlineSchool.replace('%data%', online.SCHOOL),
+                    FORMATTED_TITLE = helperVar.HTMLonlineTitle.replace('%data%', online.TITLE),
+                    FORMATTED_DATES = helperVar.HTMLonlineDates.replace('%data%', online.DATES),
+                    FORMATTED_URL = helperVar.HTMLonlineURL.replace('%data%', online.URL),
+                    FORMATTED_TITLE_SCHOOL = FORMATTED_TITLE + FORMATTED_SCHOOL;
+                FORMATTED_TITLE_SCHOOL = FORMATTED_TITLE_SCHOOL.replace('#', online.URL),
+                    $('#education').append(helperVar.HTMLonlineClasses);
+                $('.education-entry:last').append(FORMATTED_TITLE_SCHOOL);
+                $('.education-entry:last').append(FORMATTED_DATES);
+                // $('.education-entry:last').append(FORMATTED_URL);
+            });
+        }
+    },
+
+    PROJECTS: {
+        PROJECTS: [
+            {
+                TITLE: 'Theory of Constraints Simulation',
+                DATES: '2016',
+                DESCRIPTION: 'This is an on-going project that I\'m working on for a fellow instructor who teaches ' +
+                'Theory of Constraints and Critical Chain theory for logistics management.  The simulator makes use ' +
+                'of the Chart.js library to dynamicly build graphs based on simulation data.  It also makes use of jQuery ' +
+                'to ease managment of input data.  The design of the GUI has not been a priority to this point as the focus ' +
+                'has been on the underlying algorithms and data structures.',
+                URL: 'http://www.gamisweb.com/toc',
+                GITURL: 'https://github.com/corigami/ToC-Sim',
+                IMAGES: [
+                    'images/toc_500.JPG'
+                ]
+            },
+            {
+                TITLE: 'RSS Feed - Test Driven Development Using Jasmine',
+                DATES: '2016',
+                DESCRIPTION: 'This project highlights the use of Jasmine to conduct unit tests on a basic web application.  ' +
+                'Tests for functionality of have certain functions have been implemented.  Additionally, tests have been created ' +
+                'for functions that do not yet exist, highlighting the red-green-refactor approach to development.',
+                URL: 'http://www.gamisweb.com/tdd',
+                GITURL: 'https://github.com/corigami/FEND-feedreader-testing',
+                IMAGES: [
+                    'images/rss_500.JPG'
+                ]
+            },
+
+            {
+                TITLE: 'Neighborhod Map',
+                DATES: '2016',
+                DESCRIPTION: 'This project displays multiple lessons learned throughout my Udacity Nanodegree program.  ' +
+                'It encompasses responsive design, asynchronous third-party API calls, error-handling, jQuery, and ' +
+                'separation of concerns, leveraging MVVM and the Knockout.js library.  You can use the filter box to ' +
+                'narrow down number of entries.  Clicking on either the side menu or a pin will display additional ' +
+                ' information from Wikipedia if available.',
+                URL: 'http://www.gamisweb.com/map',
+                GITURL: 'https://github.com/corigami/FEND-neighborhood-map',
+                IMAGES: [
+                    'images/map_300.JPG'
+                ]
+            },
+            {
+                TITLE: 'OMNet++ UAV P2P File Replication Simulation',
+                DATES: '2015',
+                DESCRIPTION: 'This is the simulation that was produced as part of my Master\'s Thesis examining ' +
+                'the impact of file replication in a UAV swarm utilizing reactive MANET protocols. The simulation was ' +
+                'produced utilizing OMNeT++ v4.6 and is written in C++. It simulates a swarm of UAVs generating files at ' +
+                'a variable rate and replicates them using a implementation of the ORION transfer protocol for MANETs. ' +
+                'A master node then queries the swarm for random files and logs the data.' +
+                'Results were analyzed with R, and documented in my thesis which was generated using Latex.' +
+                'Thesis can viewed at the Defense Technical Information Center by clicking on the Title link.',
+                URL: 'http://www.dtic.mil/docs/citations/ADA615415',
+                EMBED: '<iframe src="https://mix.office.com/embed/zvbylas7wt19" frameborder="0" allowfullscreen></iframe>',
+                GITURL: 'https://github.com/corigami/thesis_orion_omnet',
+                IMAGES: [
+                    'images/omnet.jpg'
+                ]
+            },
+            {
+                TITLE: 'Unity 3D Ant Colony Optimization',
+                DATES: '2014',
+                DESCRIPTION: 'This is a project that I worked on for my advanced algorithms class. It was creates created ' +
+                'in Unity 3d and written with C#. It demonstrates the usage of an Ad hoc On-demand Distance Vector (AODV) ' +
+                'routing protocol, as well as an Ant Colony Optimization and Genetic algorithm to produce a minimally connected ' +
+                'dominated set (MCDS) of nodes to be utilized in a mobile ad-hoc network. Network traffic is notional, and is ' +
+                'abstracted to just send/receive mechanics (no OSI stack, headers, etc).  As a part of the project, I engineered ' +
+                'the project to be extendable to use different mobility and network models in an to implement SOLID design principles.',
+                URL: 'https://github.com/corigami/Simulator',
+                EMBED: '<iframe src="https://mix.office.com/embed/ts3kisoddce5" frameborder="0" allowfullscreen></iframe>',
+                GITURL: 'https://github.com/corigami/Simulator',
+                IMAGES: [
+                    'images/unityACO_sq_300.jpg',
+                    'images/unityACO_300_b.jpg'
+                ]
+            },
+            {
+                TITLE: 'Arcade Game Clone',
+                DATES: '2015',
+                DESCRIPTION: 'This was an assignment for the Udacity Front-End Web Developemnt Nanodegree. ' +
+                ' The purpose was to showcase Object Oriented Programming in JavaScript. It was useful to see' +
+                ' how OOP worked in JavaScript versus other languages that I was used to such as C++ and Java.' +
+                ' Hopefully I\'ll get around to adding more functionality as time permits, but in its current state' +
+                ' it has a working movement controller, collision detection with enemies and obstacles and level progression.',
+                URL: 'http://www.gamisweb.com/frogger',
+                GITURL: 'https://github.com/corigami/frontend-nanodegree-arcade-game',
+                IMAGES: [
+                    'images/frogger_sq_300.jpg'
+                ]
+            },
+            {
+                TITLE: 'Career Development Course Tester',
+                DATES: '2005ish',
+                DESCRIPTION: 'If you want an example of bad code, look no further! This was written in my early undergraduate ' +
+                'days before I had any clue what I was doing. I did manage to produce a text parser to read in pre-formated text to ' +
+                'populate the question database. Sadly, after 10 years of use, this program is still being used by some workcenters ' +
+                'in my old Avionics career field. It implements random ordering of questions and answers and produces a text report output.',
+                GITURL: 'https://github.com/corigami/gamitester',
+                URL: 'https://github.com/corigami/gamitester',
+                IMAGES: [
+                    'images/cdcTester_sq_300.jpg',
+                    'images/cdcTester_sq_300b.jpg',
+                    'images/cdcTester_sq_300c.jpg'
+                ]
+            }
+        ],
+    },
+
+};
